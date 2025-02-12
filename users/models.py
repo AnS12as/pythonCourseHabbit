92521ac1d8cc9c4c5b1a8062f110a14a1951b4f2
anastasiyasamokhina <anastasiya0625@gmail.com>
2025-02-12 16:04:23 +0300

    Final fixes and deployment check

--- users/models.py
+++ users/models.py
@@ -19,6 +19,8 @@ class CustomUserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser):
+    username = models.CharField(max_length=150, default="default_username")
+    password = models.CharField(max_length=255)
     email = models.EmailField(unique=True)  # Ensure email is unique
     phone = models.CharField(max_length=15, blank=True, null=True)
     city = models.CharField(max_length=100, blank=True, null=True)
