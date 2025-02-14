830828410c185fe679520090c035733c8fe288c8
anastasiyasamokhina <anastasiya0625@gmail.com>
2025-02-14 10:16:17 +0300

    Final fixes and deployment check

--- users/models.py
+++ users/models.py
@@ -58,3 +58,14 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
